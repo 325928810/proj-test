9924e4d566ebfb095c0f4d712e39553a196f129d
325928810 <mashcanta.project@gmail.com>
2024-05-06 22:57:26 +0300

    second

--- client/src/common/logo.jsx
+++ client/src/common/logo.jsx
@@ -9,6 +9,9 @@ export default function Logo() {
         <br/>  <br/>
         <></>
         <h1>aaa</h1>
+        <h1>aaa</h1>
+
+
         <div className="card flex justify-content-center">
           
             <Image src="http://localhost:1133/logoV.png" indicatorIcon={icon} alt="Image" preview width="1000" />
